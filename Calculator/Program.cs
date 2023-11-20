341c835099a959b2b481e15b6751c23309b2872c
unsighera <eddy.weston@bk.ru>
2023-11-20 21:16:51 +0300

    Добавьте файлы проекта.

---
+++ Calculator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите выражение: ");
+        string expression = Console.ReadLine();
+
+        try
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("", typeof(double), expression);
+            DataRow row = table.NewRow();
+            table.Rows.Add(row);
+
+            double result = (double)row[0];
+
+            Console.WriteLine("Результат: " + result);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ошибка: " + ex.Message);
+        }
+    }
+}
